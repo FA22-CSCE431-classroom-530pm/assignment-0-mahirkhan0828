b0d8ba7121c956004884beff44b8b2d172bdf9d5
root <root@localhost>
2022-09-12 15:48:05 +0000

    Added tests for author, price, date attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,36 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+
+# Integration test for author
+RSpec.describe 'Creating a book with author attribute', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'J.K. Rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('J.K. Rowling')
+  end
+end
+
+# Integration test for price
+RSpec.describe 'Creating a book with price attribute', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: 10
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(10)
+  end
+end
+
+# Integration test for published date
+RSpec.describe 'Creating a book with date attribute', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Date', with: 'Sat, 10 Nov 2007'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Sat, 10 Nov 2007')
+  end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,52 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+# Unit test for author
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'J.K. Rowling')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+# Unit test for price
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(price: 10)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+# Unit test for published date
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(publishedDate: "Sat, 10 Nov 2007")
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
